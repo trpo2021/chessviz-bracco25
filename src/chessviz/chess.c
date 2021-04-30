378e3834dc2b121fe99140a7089f5a97f43e11d7
bracco25 <bracco25@users.noreply.github.com>
2021-04-30 12:14:18 +0700

    Create src/chessviz/

---
+++ src/chessviz/chess.c
@@ -0,0 +1,36 @@
+#include <libchessviz/chess_f.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char board[9][9];
+
+    create_board(board);
+    display(board);
+
+    char qwer[10];
+
+    FILE* turn;
+    turn = fopen("bin/turn.txt", "r");
+
+    int check = 1;
+    int gr = 1;
+
+    while (!feof(turn)) {
+        fscanf(turn, "%s", qwer);
+
+        gr = right_intput(qwer);
+        if (gr != 1) {
+            return 0;
+        }
+
+        check = move(board, qwer);
+
+        if (check != 1) {
+            return 0;
+        }
+    }
+    fclose(turn);
+    return 0;
+}
